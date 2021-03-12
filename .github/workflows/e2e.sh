6cd18d68415ae28c69bafd81834ccb58072f3e12
Emilien Escalle <emilien.escalle@escemi.com>
2021-03-12 12:16:03 +0100

    ci: fix e2e tests for not core packages

--- .github/workflows/e2e.sh
+++ .github/workflows/e2e.sh
@@ -5,7 +5,7 @@ set -e
 PACKAGE_DIR="$(pwd)/packages"
 for PACKAGE in $(ls $PACKAGE_DIR/)
 do
-    echo "Test @ts-dev-tool/$PACKAGE:";
+    echo "\nTest @ts-dev-tool/$PACKAGE:\n-----------------------------\n";
 
     TEST_DIR="/tmp/e2e-project-$PACKAGE";
     rm -fr "$TEST_DIR"
@@ -19,9 +19,25 @@ do
     yarn tsc --init;
     yarn add --dev "file:/$PACKAGE_DIR/$PACKAGE";
 
+    # Force core dist
+    if [ "$PACKAGE" != "core" ]; then
+        CORE_DIST_DIR_PATH="$TEST_DIR/node_modules/@ts-dev-tools/core/dist"
+        TS_DEV_TOOLS_BIN_PATH="$TEST_DIR/node_modules/.bin/ts-dev-tools"
+
+        rm -fr "$CORE_DIST_DIR_PATH"
+        
+        cp -R "$PACKAGE_DIR/core/dist" "$CORE_DIST_DIR_PATH"
+        ln -sf "$CORE_DIST_DIR_PATH/bin.js" "$TS_DEV_TOOLS_BIN_PATH"
+    fi;
+
     sudo chmod +x node_modules/.bin/ts-dev-tools
     yarn ts-dev-tools install;
 
     yarn lint;
     yarn tsc src/*.ts;
+
+    # Clean
+    rm -fr "$TEST_DIR"
+
+    echo "\nTest of @ts-dev-tool/$PACKAGE succeed!\n";
 done
